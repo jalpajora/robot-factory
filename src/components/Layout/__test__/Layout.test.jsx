3536862a17ee68f7b756cf8eaebba57765f5d029
jalpajora <jezamae.alpajora@gmail.com>
2021-09-23 23:18:56 +0800

    test: update layout test script

--- src/components/Layout/__test__/Layout.test.jsx
+++ src/components/Layout/__test__/Layout.test.jsx
@@ -1,18 +1,10 @@
 import { render, RenderResult } from '@testing-library/react';
 
-import Layout from '../Layout';
+import Layout from '..';
 
 let Component: RenderResult;
 beforeEach(() => {
   Component = render(<Layout />);
-});
-
-test('layout must have a logo', () => {
-  expect(Component.getByAltText('company logo')).toBeInTheDocument();
-});
-
-test('layout must have a logout', () => {
-  expect(Component.getByTestId('logout-btn')).toBeInTheDocument();
 });
 
 test('layout must have an app header', () => {
@@ -23,18 +15,26 @@ test('layout must have an app footer', () => {
   expect(Component.getByRole('footer')).toBeInTheDocument();
 });
 
+test('layout must have a logo', () => {
+  expect(Component.getByAltText('company logo')).toBeInTheDocument();
+});
+
+test('layout must have a logout', () => {
+  expect(Component.getByTestId('logout-btn')).toBeInTheDocument();
+});
+
 test('layout must have a "QA" menu link', () => {
-  expect(Component.getByRole('link', { label: 'QA' })).toBeInTheDocument();
+  expect(Component.getByText('QA', { selector: 'a' })).toBeInTheDocument();
 });
 
 test('layout must have a "Ready to Ship" link', () => {
   expect(
-    Component.getByRole('link', { label: 'Ready to Ship' })
+    Component.getByText('Ready to Ship', { selector: 'a' })
   ).toBeInTheDocument();
 });
 
 test('layout must have a "Shipping" link', () => {
   expect(
-    Component.getByRole('link', { label: 'Shipping' })
+    Component.getByText('Shipping', { selector: 'a' })
   ).toBeInTheDocument();
 });
