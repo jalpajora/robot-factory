409301c1b264fc74387dc870dd228ee7af2f4f23
jalpajora <jezamae.alpajora@gmail.com>
2021-09-23 22:03:09 +0800

    feat: initial app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,8 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+test('renders app', () => {
+  const { getByTestId } = render(<App />);
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByTestId('app')).toBeInTheDocument();
 });
