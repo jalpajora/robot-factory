eedcc6375190f41c81919be9a834f6f7523b83eb
jalpajora <jezamae.alpajora@gmail.com>
2021-09-24 23:57:18 +0800

    test: update test scripts for dashboard routes

--- src/components/App/__test__/App.test.tsx
+++ src/components/App/__test__/App.test.tsx
@@ -1,39 +1,92 @@
-import { render, RenderResult } from '@testing-library/react';
+import { render, RenderResult, fireEvent } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { waitFor } from '@testing-library/dom';
+
 import App from '..';
 
-let Component: RenderResult;
-beforeEach(() => {
-  Component = render(<App />);
+describe('App display', () => {
+  let Component: RenderResult;
+  beforeEach(() => {
+    Component = render(<App />);
+  });
+
+  test('renders app', () => {
+    expect(Component.getByTestId('app')).toBeInTheDocument();
+  });
+
+  test('layout must have an app header', () => {
+    expect(Component.getByRole('navigation')).toBeInTheDocument();
+  });
+
+  test('layout must have a logo', () => {
+    expect(Component.getByAltText('company logo')).toBeInTheDocument();
+  });
+
+  test('layout must have a logout', () => {
+    expect(Component.getByTestId('logout-btn')).toBeInTheDocument();
+  });
+
+  test('layout must have a "QA" menu link', () => {
+    expect(Component.getByText('QA', { selector: 'a' })).toBeInTheDocument();
+  });
+
+  test('layout must have a "Ready to Ship" link', () => {
+    expect(
+      Component.getByText('Ready to Ship', { selector: 'a' })
+    ).toBeInTheDocument();
+  });
+
+  test('layout must have a "Shipping" link', () => {
+    expect(
+      Component.getByText('Shipping', { selector: 'a' })
+    ).toBeInTheDocument();
+  });
 });
 
-test('renders app', () => {
-  expect(Component.getByTestId('app')).toBeInTheDocument();
-});
+describe('When user clicks link', () => {
+  let Component: RenderResult;
+  beforeEach(() => {
+    Component = render(
+      <MemoryRouter>
+        <Route path='/'>
+          <App />
+        </Route>
+      </MemoryRouter>
+    );
+    return Component;
+  });
 
-test('layout must have an app header', () => {
-  expect(Component.getByRole('navigation')).toBeInTheDocument();
-});
+  test('Logo link should render initial dashboard', () => {
+    expect(Component.getByTestId('db-inventory')).toBeInTheDocument();
+  });
 
-test('layout must have a logo', () => {
-  expect(Component.getByAltText('company logo')).toBeInTheDocument();
-});
+  test('"QA" link should render "QA" dashboard', async () => {
+    const { getByTestId, getByText } = Component;
+    expect(getByTestId('db-inventory')).toBeInTheDocument();
 
-test('layout must have a logout', () => {
-  expect(Component.getByTestId('logout-btn')).toBeInTheDocument();
-});
+    fireEvent.click(getByText('QA', { selector: 'a' }));
+    await waitFor(() => {
+      expect(getByTestId('db-qa')).toBeInTheDocument();
+    });
+  });
 
-test('layout must have a "QA" menu link', () => {
-  expect(Component.getByText('QA', { selector: 'a' })).toBeInTheDocument();
-});
+  test('"Ready to Ship" link should render "Ready to Ship" dashboard', async () => {
+    const { getByTestId, getByText } = Component;
+    expect(getByTestId('db-inventory')).toBeInTheDocument();
 
-test('layout must have a "Ready to Ship" link', () => {
-  expect(
-    Component.getByText('Ready to Ship', { selector: 'a' })
-  ).toBeInTheDocument();
-});
+    fireEvent.click(getByText('QA', { selector: 'a' }));
+    await waitFor(() => {
+      expect(getByTestId('db-ready-to-ship')).toBeInTheDocument();
+    });
+  });
 
-test('layout must have a "Shipping" link', () => {
-  expect(
-    Component.getByText('Shipping', { selector: 'a' })
-  ).toBeInTheDocument();
+  test('"Shipping" link should render "Shipping" dashboard', async () => {
+    const { getByTestId, getByText } = Component;
+    expect(getByTestId('db-inventory')).toBeInTheDocument();
+
+    fireEvent.click(getByText('QA', { selector: 'a' }));
+    await waitFor(() => {
+      expect(getByTestId('db-shipping')).toBeInTheDocument();
+    });
+  });
 });
--- src/components/Dashboard/__test__/Dashboard.test.jsx
+++ src/components/Dashboard/__test__/Dashboard.test.jsx
@@ -9,15 +9,3 @@ beforeEach(() => {
 test('renders dashboard', () => {
   expect(Component.getByRole('main')).toBeInTheDocument();
 });
-
-test('should render "QA" dashboard', () => {
-  expect(Component.getByTestId('db-qa')).toBeInTheDocument();
-});
-
-test('should render "Ready to Ship" dashboard', () => {
-  expect(Component.getByTestId('db-ready-to-ship')).toBeInTheDocument();
-});
-
-test('should render "Shipping" dashboard', () => {
-  expect(Component.getByTestId('db-shipping')).toBeInTheDocument();
-});
